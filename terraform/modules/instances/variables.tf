52394a6cf1fc8035b0c89b0334b83c318500e4e0
blachester <blachester@gmail.com>
2022-07-30 19:48:28 +0000

    add variable

---
+++ terraform/modules/instances/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  type    = string
+  default = "us-central"
+}
+
+
+variable "zone" {
+  type   = string
+  default = "us-central1-a"
+}
+
+variable "project_id" {
+  type    = string
+  default = "qwiklabs-gcp-03-48195c3f6b51"
+}
